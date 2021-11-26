e710e62db668374d55402c488bfa37ec92f5249a
hbhushan3 <hbhushan3@gmail.com>
2021-11-26 15:49:54 -0600

    tutoring info table created

---
+++ tutoring_info.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Himanshu">
+
+        <title>Tutor Info</title>
+
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <a href="index.html">Go back to homepage</a> <br>
+        <a href="form.php">Are you a tutor? Enter your availibilty here</a>
+        <h2>List of tutors with contact information and their availibility this week</h2>
+
+        <table id="tutor_table">
+            <tr>
+                <th>Email</th>
+                <th>Name</th>
+                <th>Day</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>M</th>
+                <th>T</th>
+                <th>W</th>
+                <th>Th</th>
+                <th>F</th>
+    </body>
+</html>
